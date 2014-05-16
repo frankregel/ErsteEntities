e6a561aa9fb89646a60d3bd82e1aceee48fc7756
frankregel <frankregel@Franks-Mac-mini.local>
2014-05-16 09:43:15 +0200

    Initial Commit

---
+++ ErsteEntitiesTests/ErsteEntitiesTests.m
@@ -0,0 +1,34 @@
+//
+//  ErsteEntitiesTests.m
+//  ErsteEntitiesTests
+//
+//  Created by Frank Regel on 16.05.14.
+//  Copyright (c) 2014 appanatics. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ErsteEntitiesTests : XCTestCase
+
+@end
+
+@implementation ErsteEntitiesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
